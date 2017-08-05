9939b12fbe058397c2feaff1498ada5c8cb6e9b5
klmz <kaalaamaazoo@gmail.com>
2017-08-05 13:33:28 +0200

    Add a templating engine

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import http from 'http';
 import express from 'express';
+import mustacheExpress from 'mustache-express';
 import cors from 'cors';
 import morgan from 'morgan';
 import bodyParser from 'body-parser';
@@ -9,6 +10,11 @@ import api from './api';
 import config from './config.json';
 
 let app = express();
+// Register '.mustache' extension with The Mustache Express
+app.engine('mustache', mustacheExpress());
+app.set('view engine', 'mustache');
+app.set('views', __dirname + '/views');
+
 app.server = http.createServer(app);
 
 // logger
@@ -16,25 +22,35 @@ app.use(morgan('dev'));
 
 // 3rd party middleware
 app.use(cors({
-	exposedHeaders: config.corsHeaders
+    exposedHeaders: config.corsHeaders
 }));
 
 app.use(bodyParser.json({
-	limit : config.bodyLimit
+    limit: config.bodyLimit
 }));
 
+app.use(express.static(__dirname + '/public'))
+
 // connect to db
-initializeDb( db => {
+initializeDb(db => {
 
-	// internal middleware
-	app.use(middleware({ config, db }));
+    // internal middleware
+    app.use(middleware({
+        config,
+        db
+    }));
 
-	// api router
-	app.use('/api', api({ config, db }));
-
-	app.server.listen(process.env.PORT || config.port, () => {
-		console.log(`Started on port ${app.server.address().port}`);
-	});
+    // api router
+    app.use('/api', api({
+        config,
+        db
+    }));
+    app.use('/', (req, res) => {
+        res.render("index");
+    })
+    app.server.listen(process.env.PORT || config.port, () => {
+        console.log(`Started on port ${app.server.address().port}`);
+    });
 });
 
 export default app;
